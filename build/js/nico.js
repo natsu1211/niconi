a86a52a3c79d3b301bec0b28b02527858ccfefc6
natsu1211 <longyutao1211@gmail.com>
2016-03-01 21:10:56 +0800

    use inner js temp

--- build/js/nico.js
+++ build/js/nico.js
@@ -3,13 +3,14 @@ var loginNico = function(mail, pwd){
     var body = "mail"
 }
 
-var getVideoUrl = function(){
-    var videoIdText = document.getElementById("video_id")
-    var API_URL = "http://flapi.nicovideo.jp/api/getflv/"
-    req = new XMLHttpRequest;
-    req.open("GET", API_URL+videoIdText, false);
+var getVideoUrl = function(id){
+    //var videoIdText = window.document.getElementById("video_id");
+    var API_URL = "http://flapi.nicovideo.jp/api/getflv/";
+    req = new XMLHttpRequest();
+    req.open("GET", API_URL + id, true);
     req.withCredentials = true;
     req.send(null);
     var response_data = req.responseText;
     alert(response_data);
+    return response_data;
 }
